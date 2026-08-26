e55056610476bfd1b8042f82c8b4e793182e2dc3
abhi458435 <ak8827102@gmail.com>
2026-08-26 22:16:23 +0530

    Added my variable.py file

---
+++ Python/1st Semester/variable.py
@@ -0,0 +1,140 @@
+#  18.
+# What values will the following variables refer to?
+
+# ```python
+# name = "Rahul"
+# age = 18
+# city = "Patna"
+# #ans:- ```
+# name-Rahul
+# age-18
+# city-Patna
+
+#  19.
+# What is the final value of `age`?
+
+# ```python
+# age = 18
+# age = 19
+# ```
+
+# Explain why.
+#Ans:-Because Python refers new value instead of old value
+
+# 20.
+# What is the final value of `name`?
+
+# ```python
+# name = "Rahul"
+# name = "Amit"
+# name = "Riya"
+# ```
+#Ans:-Riya
+21.
+# Explain what happens in this program:
+
+# ```python
+# student_name = "Rahul"
+# student_age = 18
+
+# student_age = 19
+# ```
+# Ans:- In this program student name is Rahul and student age is 19 
+22.
+# What values will `a`, `b`, and `c` refer to?
+
+# ```python
+# a, b, c = 10, 20, 30
+#Ans:-
+# a=10
+# b=20
+# c=30
+# 23.
+# What values will `x`, `y`, and `z` refer to?
+
+# ```python
+# x = y = z = 100
+# ```
+# Ans:-x=100,y=100,z=100
+## D. Practical Problems
+
+# ### 24.
+# Create variables for the following information:
+
+# - Your name
+# - Your age
+# - Your city
+# Ans:-
+# your_name="Abhishek Kumar"
+# your_age=18
+# your_city="Begusarai"
+# print(your_name)
+# print(your_age)
+# print(your_city)
+25.
+# Create variables for:
+
+# - Student name
+# - Student roll number
+# - Student branch
+# Ans:-
+# student_name="Abhishek Kumar"
+# student_roll_number=57
+# student_branch="Cse"
+
+# print(student_name)
+# print(student_roll_number)
+# print(student_branch)
+
+# 26.
+# Write a program that assigns a value to a variable called `marks`, then reassigns a new value to `marks`.
+
+# Explain the value before and after reassignment.
+
+# marks=55.8
+# marks=87.6
+# print(marks)
+# # before=55.8, new value=87.6
+# 27.
+# Use multiple assignment to create the following variables in one statement:
+
+# ```text
+# name → "Rahul"
+# age → 18
+# city → "Patna"
+# name,age,city="Rahul",18,"Patna"
+# print(name,age,city)
+# 28.
+# Use one statement to assign the value `0` to three variables:
+
+# ```text
+# x
+# y
+# z
+# x=y=z=0
+# print(x,y,z)
+# 29.
+# The following code contains invalid variable names:
+
+# ```python
+# 1student = "Rahul"
+# student name = "Rahul"
+# class = "B.Tech"
+# # ```
+# student1="Rahul"
+# student_name="Rahul"
+# class_="B.Tech"
+# print(student1)
+# print(student_name)
+# print(class_)
+# 30.
+# Create a small Python program that stores a student's name and age, reassigns the age to a new value, and then displays the current values. Use:
+# - A useful comment
+# - Meaningful variable names
+# - `snake_case`
+# - Reassignment
+# student_name="Abhishek Kumar"
+# student_age=18
+# student_age=19
+# print(student_name)
+# print(student_age)
