52a0d1d3e27b8c3226f600cddc0c6c27bddda864
abhi458435 <ak8827102@gmail.com>
2026-09-04 00:22:00 +0530

    Modify my index.py File

--- Subjects/Python/1st Semester/Assignment.md/index.py
+++ Subjects/Python/1st Semester/Assignment.md/index.py
@@ -1,75 +1,315 @@
-##Question 1
-name="Abhishek"
-print("Name:",name)
-##Question 2
-student_name="Abhishek"
-print("Student Name:",student_name)
-##Question 3
-student_name="Rahul"
-print("Student Name:",student_name)
-##Question 4
-student_name1="Abhishek Kumar"
-print("Student Name:",student_name1)
-##Question 5
-student_name="Rohan"
-print("Student Name:",student_name)
-##Question 6
-student_name="Neha"
-student_age=19
-print("Name:",student_name)
-print("Age:",student_age)
-##Question 7
-student_name1="Karan"
-student_name="Raj"
-print("Student 1:",student_name1)
-print("Student 2:",student_name)
-##Question 8
-student_name="Priya"
-student_age=19
-print("Name:",student_name)
-print("Age:",student_age)
-##Question 9
-name2="Amit"
-student_age=18
-print("Name:",name2)
-print("Age:",student_age)
-##Question 10
-college_name="ABC college"
-student_name="Meera"
-print("College:",college_name)
-print("Student:",student_name)
-##Q-11
-first_name="Abhishek"
-last_name="Kumar"
-print("Full Name:",first_name,last_name)
-##Q-12
-##Ans:-Same as above question no 11
-##Q-13
-name='Abhishek Kumar'
-age=18
-course='B.Tech'
-city="Patna"
-print("My name is",name+".","I am",age,"years old.","I am studying",course,"and I live in",city+".")
-##Q-14
-name="Aman Kumar"
-roll_number=101
-branch="Computer Science"
-semester=1
-print("My name is ",name+".","My roll number is",roll_number,"I am studying",branch,"in semester",semester)
-##Q-15
-name="Abhishek Kumar"
-age=18
-gender="Male"
-city="Begusarai"
-print("My name is",name+".","and I am ",age,"years old","I am",gender+".","I live in ",city )
-##Q-16
-brand="Samsung"
-model="Galaxy A55"
-ram="8 GB"
-storage="256 GB"
-print('I have a',brand,model,"with",ram,"RAM and",storage,"storage.")
+# ##Question 1
+# name="Abhishek"
+# print("Name:",name)
+# ##Question 2
+# student_name="Abhishek"
+# print("Student Name:",student_name)
+# ##Question 3
+# student_name="Rahul"
+# print("Student Name:",student_name)
+# ##Question 4
+# student_name1="Abhishek Kumar"
+# print("Student Name:",student_name1)
+# ##Question 5
+# student_name="Rohan"
+# print("Student Name:",student_name)
+# ##Question 6
+# student_name="Neha"
+# student_age=19
+# print("Name:",student_name)
+# print("Age:",student_age)
+# ##Question 7
+# student_name1="Karan"
+# student_name="Raj"
+# print("Student 1:",student_name1)
+# print("Student 2:",student_name)
+# ##Question 8
+# student_name="Priya"
+# student_age=19
+# print("Name:",student_name)
+# print("Age:",student_age)
+# ##Question 9
+# name2="Amit"
+# student_age=18
+# print("Name:",name2)
+# print("Age:",student_age)
+# ##Question 10
+# college_name="ABC college"
+# student_name="Meera"
+# print("College:",college_name)
+# print("Student:",student_name)
+# ##Q-11
+# first_name="Abhishek"
+# last_name="Kumar"
+# print("Full Name:",first_name,last_name)
+# ##Q-12
+# ##Ans:-Same as above question no 11
+# ##Q-13
+# name='Abhishek Kumar'
+# age=18
+# course='B.Tech'
+# city="Patna"
+# print("My name is",name+".","I am",age,"years old.","I am studying",course,"and I live in",city+".")
+# ##Q-14
+# name="Aman Kumar"
+# roll_number=101
+# branch="Computer Science"
+# semester=1
+# print("My name is ",name+".","My roll number is",roll_number,"I am studying",branch,"in semester",semester)
+# ##Q-15
+# name="Abhishek Kumar"
+# age=18
+# gender="Male"
+# city="Begusarai"
+# print("My name is",name+".","and I am ",age,"years old","I am",gender+".","I live in ",city )
+# ##Q-16
+# brand="Samsung"
+# model="Galaxy A55"
+# ram="8 GB"
+# storage="256 GB"
+# print('I have a',brand,model,"with",ram,"RAM and",storage,"storage.")
+# ##Q-17
+# name="Rohit Kumar"
+# job_role="Software Developer"
+# company="ABC Technologies"
+# experience="2 years"
+# print("My name is",name+".","I work as a",job_role,"at",company,"and I have",experience,"experience.")
+# ##Q-18
+# college_name="XYZ Institute of Technology"
+# course="B.Tech"
+# branch='Information Technology'
+# city='Ahmedabad'
+# print("I am studying",course,"in",branch,"at",college_name,"in",city+".")
+# ##Q-19
+# product="Laptop"
+# brand="HP"
+# price=55000
+# quantity=1
+# print("I bought",quantity,brand,product,"for",price,"rupees")
+# ##Q-20
+# name='Rahul Raj'
+# fathers_name='Rajesh Kumar'
+# mothers_name='Sunita Kumari'
+# city='Patna'
+# print('My name is',name+'.',"My father's name is",fathers_name+".","My mother's name is",mothers_name+".","I live in ",city+".")
+# ##Q-21
+# name='Neha Sharma'
+# subject='Python Programing'
+# marks=88
+# grade='A'
+# percentage=88.0
+# print(name,'scored',marks,'marks in',subject+".",'Her grade is',grade,'and her percentage is',str(percentage)+".")
+# ##Q-22
+# brand="HP"
+# model="Pavilion 15"
+# processor='Intel Core i5'
+# ram="16 GB"
+# storage="512 GB SSD"
+# price=65000
+# print("I have an",brand,model,"with",str(processor)+",",ram,"RAM",',',str(storage)+",","and it costs",price,"rupees.")
+# ##Q-23
+# movie="3 Idiots"
+# genre='Comedy Drama'
+# rating=8.4
+# language="Hindi"
+# release_year=2009
+# print(movie,'is a',genre,'movie in',language+".",'It was released in',release_year,'and has a ratings of',str(rating)+".")
+# ##Q-24
+# name='Arjun Patel'
+# age=19
+# college_name='ABC Institute of Technology'
+# branch="Computer Science"
+# semester=1
+# city='Ahmedabad'
+# percentage=87.5
+# programing_language='Python'
+# print('My name is',name+".","I am",age,"years old and I am studying",branch,'in semester',semester,'at',college_name,'in',city+".",'I scored',percentage,"percentage",'and my favorite programming language is',programing_language+".")
+# ##Q-25
+# name = "Ravi"
+# age = 18
+# marks = 90.5
+# passed = True
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Passed:", passed)
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(passed))
+# ##Q-26
+# name = "Ravi"
+# age = 18
+# marks = 90.5
+# passed = True
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+# print("Passed:", passed)
+
+# print(type(name))
+# print(type(age))
+# print(type(marks))
+# print(type(passed))
+# ##Q-27
+# student_name = "Karan"
+# student_age = 18
+# student_marks = 87.5
+
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+
+# print("Student Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+# ##Q-28 same as previous question
+# ##Topic:4 Syntax Errors and Debugging
+# #Q-29
+# name = "Riya"
+# age = 20
+
+# print("Name:", name)
+# print("Age:", age)
+# #Q-30
+# name = "Aman"
+# age = 18
+
+# print("Name:", name)
+# print("Age:", age)
+# #Q-31
+# student_name = "Rohan"
+# student_age = 19
+# student_city = "Ahmedabad"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("City:", student_city)
+# #Q-32
+# student_name = "Neha"
+# student_age = 20
+# student_city = "Vadodara"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("City:", student_city)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("City Type:", type(student_city))
+# #Q-33
+# student_name = "Amit"
+# student_age = 18
+# student_college = "XYZ College"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("College:", student_college)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# #Q-34
+# student_name = "Meera"
+# student_age = 20
+# student_college = "ABC College"
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("College:", student_college)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("College Type:", type(student_college))
+# #Q-35
+# student_name = "Raju"
+# student_age = 18
+# student_marks = 92.5
+# is_passed = True
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Passed:", is_passed)
+
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+# print("Passed Type:", type(is_passed))
+# #Q-36
+# student_name = "Raju"
+# student_age = 18
+# student_marks = 92.5
+# is_passed = True
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Passed:", is_passed)
+# #Q-37
+# name = "Amit"
+# age = 18
+# marks = 91.5
+
+# print("Name:", name)
+# print("Age:", age)
+# print("Marks:", marks)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+# #Q-38
+# college_name = "ABC College"
+# student_name = "Riya"
+# student_age = 19
+
+# print("College:", college_name)
+# print("Student:", student_name)
+# print("Age:", student_age)
+# print("College Type:", type(college_name))
+# #Q-39
+# class_name="B.Tech"
+# student_name = "Ravi"
+# print("Class:", class_name)
+# print("Student:", student_name)
+# #Q-40
+# student_name = "Ravi"
+# student_age = 18
+# student_marks = 89.5
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+# #Q-41
+# student_name = "Ravi"
+# student_age = 18
+# student2_marks = 89.5
+
+# print("Name:", student_name)
+# print("Age:", student_age)
+# print("Marks:", student_marks)
+# print("Name Type:", type(student_name))
+# print("Age Type:", type(student_age))
+# print("Marks Type:", type(student_marks))
+# #Q-42
+# name = "Karan"
+# age = "18"
+# marks = 87.5
+# college_name = "ABC College"
+
+# print("Name:", name)
+# print("Age:",age)
+# print("Marks:", marks)
+# print("College:", college_name)
+
+# print("Name Type:", type(name))
+# print("Age Type:", type(age))
+# print("Marks Type:", type(marks))
+# print("College Type:", type(college_name))
 
 
-
-
-
+           #THE END
