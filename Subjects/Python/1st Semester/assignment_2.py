58ad11b8ad67fbe2dbdf3b5dd9e105337365c44e
Abhishek Kumar <abhishekk.kumar.cg@gmail.com>
2026-09-06 23:11:59 +0530

    Added my assignment.md file

---
+++ Subjects/Python/1st Semester/assignment_2.py
@@ -0,0 +1,433 @@
+# #Q-01
+# age='25'
+# print(int(age))
+# print(type(int(age)))
+# #Q-02
+# marks="75.5"
+# print(float(marks))
+# print(type(float(marks)))
+# #Q-03
+# number=50
+# x=float(number)
+# print(x)
+# print(type(x))
+# #Q-04
+# marks=85.9
+# y=int(marks)
+# print(y)
+# print(type(y))
+# #Q-05
+# roll_number=101
+# z=str(roll_number)
+# print(z)
+# print(type(z))
+# #Q-06
+# p=int("18")
+# print(p)
+# print(type(p))
+# print(float("92.5"),type(float('92.5')))
+# print(str(100),type(str(100)))
+# print(int(45.8),type(int(45.8)))
+
+# #Q-07
+# a = "20"
+# b = int(a)
+
+# c = 10.8
+# d = int(c)
+
+# e = 25
+# f = str(e)
+
+# print(b)
+# print(d)
+# print(f)
+# print(type(b))
+# print(type(d))
+# print(type(f))
+# #Q-08
+# age = 19
+# new_age = age + 1
+
+# print("Age:", new_age)
+# #Q-09
+# marks='85'
+# x=int(marks)
+# bonus=5
+# final_marks=x+bonus
+# print('Final Marks:',final_marks)
+# #Q-10
+# price = "1499.50"
+# x=float(price)
+# total_amount=x+99.50
+# print('Total Amount:',total_amount)
+# #Q-11
+# a = 20
+# b = 6
+# print('Addition:',a+b)
+# print('Subtraction:',a-b)
+# print('Multiplication:',a*b)
+# print('Division:',a/b)
+# print('Floor division:',a//b)
+# print('Remainder:',a%b)
+# print('Power:',a**b)
+# #Q-12
+# a = 17
+# b = 5
+
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# #Q-13
+# result = 10 + 5 * 2
+# print(result)
+#Q-14
+result = 20 - 4 * 3 + 2
+print(result)
+#Q-15
+print(2 ** 3)
+print(3 ** 2)
+print(10 ** 2)
+#Area 
+side=5
+area_of_square=5*4
+print(area_of_square)
+#Q-16
+notebook=80
+pen=20
+pencil=10
+total_amount=notebook+pen+pencil
+print(total_amount)
+#Q-17
+notebook=3*50
+pens=2*15
+calculator=1*500
+total_bill=notebook+pens+calculator
+print("Notebook Cost:",notebook)
+print("Pen Cost:",pens)
+print('Caclulator Cost:',calculator)
+print('Total Bill:',total_bill)
+#Q-18
+students=47
+groups=students//5
+left_students=students%5
+print('Complete Groups:',groups)
+print("Students Left:",left_students)
+
+#Q-19
+python = 85
+mathematics = 78
+physics = 92
+total_marks=python+mathematics+physics
+average_marks=total_marks/3
+print('Total Marks:',total_marks)
+print("Average Marks:",average_marks)
+#Q-20
+english = 78
+mathematics = 85
+python = 92
+physics = 81
+chemistry = 74
+overall_marks=500
+My_marks=english+mathematics+python+physics+chemistry
+percentage=(My_marks*100)/overall_marks
+print('Total Marks:',My_marks)
+print("percentage:",percentage)
+#Q-21
+number=583
+ones_digit=number%10
+print("Ones Digit:",ones_digit)
+#Q-22
+tens_digit=(number//10)%10
+print('Tens Digit:',tens_digit)
+#Q-23
+hundred_digit=number//100
+print("Hundred Digit:",hundred_digit)
+#Q-24
+number1=746
+ones_digit=number1%10
+tens_digit=(number1//10)%10
+hundred_digit=number1//100
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+#Q-25
+number2=5829
+ones_digit=number2%10
+tens_digit=(number2//10)%10
+hundred_digit=(number2//100)%10
+thousands_digit=number2//1000
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+print('Thousands Digit:',thousands_digit)
+#Q-26
+number=583
+ones_digit=number%10
+tens_digit=(number//10)%10
+hundred_digit=number//100
+sum_of_digit=ones_digit+tens_digit+hundred_digit
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+print('Sum of Digits:',sum_of_digit)
+#Q-27
+number=4726
+ones_digit=number%10
+tens_digit=(number//10)%10
+hundred_digit=(number//100)%10
+thousands_digit=number//1000
+sum_of_digit=ones_digit+tens_digit+hundred_digit+thousands_digit
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+print('Thousands Digit:',thousands_digit)
+print('Sum of Digits:',sum_of_digit)
+#Q-28
+number=234
+ones_digit=number%10
+tens_digit=(number//10)%10
+hundred_digit=number//100
+product=ones_digit*tens_digit*hundred_digit
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+print('Product of Digits:',product)
+#Q-29
+
+
+
+
+
+#Q-35
+hundreds = 5
+tens = 8
+ones = 3
+number=(hundreds*100)+(tens*10)+ones
+print('Number:',number)
+#Real-Life Arithmetic Problems
+#Q-36-Simple Interest
+principal =10000
+rate = 5
+time = 2 
+simple_interest=(principal*rate*time)/100
+print("Simple Interest:",simple_interest)
+#Q-37
+length=15
+width=8
+area=length*width
+perimeter=2*(length+width)
+print('Area:',str(area)+"cm\u00B2")
+print('Perimeter:',str(perimeter)+"cm")
+#Q-38
+r=7
+pi=3.14
+area=pi*(r**2)
+print('Area:',str(area)+'cm\u00B2')
+#Q-39
+c=35
+f=(c*9/5)+32
+print(f)
+#Q-40
+seconds=367
+minutes=seconds//60
+remaining_seconds=seconds%60
+print('Minutes:',minutes)
+print('Seconds:',remaining_seconds)
+#Q-41
+total_seconds=7384
+hour=total_seconds//3600
+minutes=total_seconds//60
+seconds=total_seconds%60
+print("Hours:",hour)
+print("Minutes:",minutes)
+print("Seconds:",seconds)
+#Q-42
+basic_salary = 25000
+hra = 5000
+travel_allowance = 2500
+tax_deduction = 3000
+gross_salary=basic_salary
+net_salary=basic_salary-(hra+travel_allowance+tax_deduction)
+print(gross_salary)
+print(net_salary)
+#Q-43
+travel=120
+vehicle_average_per_litre=20
+fuel_required=travel/vehicle_average_per_litre
+total_fuel_cost=fuel_required*100
+print("Fuel Required:",fuel_required)
+print("Total Fuel cost:",total_fuel_cost)
+#Q-44
+price=2500
+discount=10
+discount_amount=price*(10/100)
+final_price=price-discount_amount
+print(discount_amount)
+print(final_price)
+#Q-45
+price = "1200"
+quantity = "4"
+x=int(price)
+y=int(quantity)
+total_price=x*y
+print("Price:",x)
+print("Quantity:",y)
+print("Total Price:",total_price)
+#Q-46
+python_marks = "85"
+math_marks = "78"
+physics_marks = "91"
+l=int(python_marks)
+m=int(math_marks)
+n=int(python_marks)
+total_marks=l+m+n
+average_marks=total_marks//3
+print("Total Marks:",total_marks)
+print("Average Marks",average_marks)
+#Q-47
+price = "1500"
+quantity = "2"
+tax_rate = "5"
+subtotal=int(price)*int(quantity)
+tax_amount=(int(quantity)*int(price))*5/100
+final_bill=subtotal-tax_amount
+print(subtotal)
+print(tax_amount)
+print(final_bill)
+#Q-48
+cost=2000
+discount=2000*(15/100)
+after_discount=cost-discount
+gst_amount=after_discount*18/100
+final_price=after_discount+gst_amount
+print(discount)
+print(after_discount)
+print(gst_amount)
+print(final_price)
+#Q-49
+price = 500
+quantity = 3
+
+total = price*quantity
+
+print("Total:", total)
+#Q-50
+marks1 = 80
+marks2 = 75
+marks3 = 90
+
+total = marks1 + marks2 + marks3
+
+print("Total Marks:", total)
+#Q-51
+a = "50"
+b = int(a)
+
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+#Q-52
+number = 99.99
+result = int(number)
+
+print(number)
+print(result)
+#Explanation:- number(99.99)  changes to nearest left side integer and decimal portion remove
+#because we change float to integers
+#Q-53
+a = 12
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+#Q-54
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+print(20 / 5 + 3)
+print(20 / (5 + 3))
+#()parentheses is follow maths bodmas rule .so according to bodmas rule we act to () instead of other mathematical operation except {},[]
+#Q-55
+number = 684
+
+a = number % 10
+b = number // 10
+c = b % 10
+d = number // 100
+
+print("Ones:",a)
+print("tens:",c)
+print("Hundreds:",d)
+#Q-56
+student_name = "Ravi"
+marks = 85
+
+total = marks + 5
+
+print("Student:", student_name)
+print("Marks:", total)
+print("Type:", type(total))
+#Q-57
+number = 746
+
+ones = number % 10
+tens = (number//10)%10
+hundreds = number // 100
+
+print("Ones:", ones)
+print("Tens:", tens)
+print("Hundreds:", hundreds)
+#Q-58
+price = 2000
+discount = 15
+
+discount_amount = price * discount / 100
+final_price = price - discount_amount
+
+print("Discount:", discount_amount)
+print("Final Price:", final_price)
+#Q-59
+student_name = "Rahul"
+marks1 = 85
+marks2 = 90
+marks3 = 78
+
+total = marks1 + marks2 + marks3
+average = total / 3
+
+print("Student:", student_name)
+print("Total Marks:", total)
+print("Average:", average)
+print("Marks Type:", type(total))
+#Q-60
+#PART-A
+number=5836
+ones_digit=number%10
+tens_digit=(number//10)%10
+hundred_digit=(number//100)%10
+thousands_digit=number//1000
+sum_of_digit=ones_digit+tens_digit+hundred_digit+thousands_digit
+print('Ones Digit:',ones_digit)
+print("Tens Digit:",tens_digit)
+print("Hundreds Digit:",hundred_digit)
+print('Thousands Digit:',thousands_digit)
+print('Sum of Digits:',sum_of_digit)
+#PART-B
+price = 1250
+quantity = 4
+discount = 10
+subtotal=quantity*price
+discount_amount=subtotal*10/100
+final_amount=subtotal-discount_amount
+print("Subtotal:",subtotal)
+
+print('Discount amount:',discount_amount)
+print("Final amount:",final_amount)
+
+
+    #THE END#
